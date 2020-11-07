d4df630397a4cb39c67f88fa78bcb44548b27731
swengeler <simon.wengeler@outlook.com>
2020-11-07 21:39:27 +0100

    Added new implementation of dataset to load stacks of images and started implementation of the DrEYEve model. Will probably have to update the dataset for the purpose of training the DrEYEve model and some of the training infrastructure to be able to train the model properly.

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -98,6 +98,20 @@ class ImageToAttentionDataset(Dataset):
         ])
 
         self.video_readers = {}
+        self.index["run_dir"] = None
+        unique_run_info = self.index.groupby(["track_name", "subject", "run"]).size().reset_index()
+        for _, row in unique_run_info.iterrows():
+            run_dir = os.path.join(self.data_root, run_info_to_path(row["subject"], row["run"], row["track_name"]))
+            if run_dir not in self.video_readers:
+                self.video_readers[run_dir] = {
+                    f"input_image_{idx}": get_indexed_reader(os.path.join(run_dir, f"{i}.mp4"))
+                    for idx, i in enumerate(self.input_names)
+                }
+                self.video_readers[run_dir]["output_attention"] = get_indexed_reader(os.path.join(
+                    run_dir, f"{self.output_name}.mp4"))
+            self.index.loc[(self.index["track_name"] == row["track_name"])
+                           & (self.index["subject"] == row["subject"])
+                           & (self.index["run"] == row["run"]), "run_dir"] = run_dir
 
     def __len__(self):
         return len(self.index.index)
@@ -106,6 +120,8 @@ class ImageToAttentionDataset(Dataset):
         # get the information about the current item
         current_row = self.index.iloc[item]
         current_frame_index = current_row["frame"]
+        current_run_dir = current_row["run_dir"]
+        """
         current_run_dir = os.path.join(self.data_root, run_info_to_path(current_row["subject"],
                                                                         current_row["run"],
                                                                         current_row["track_name"]))
@@ -118,6 +134,7 @@ class ImageToAttentionDataset(Dataset):
             }
             self.video_readers[current_run_dir]["output_attention"] = get_indexed_reader(os.path.join(
                 current_run_dir, f"{self.output_name}.mp4"))
+        """
 
         # read the frames
         inputs = [self.video_readers[current_run_dir][f"input_image_{idx}"][current_frame_index]
@@ -182,6 +199,18 @@ class ImageToControlDataset(Dataset):
         ]) for i in self.input_names]
 
         self.video_readers = {}
+        self.index["run_dir"] = None
+        unique_run_info = self.index.groupby(["track_name", "subject", "run"]).size().reset_index()
+        for _, row in unique_run_info.iterrows():
+            run_dir = os.path.join(self.data_root, run_info_to_path(row["subject"], row["run"], row["track_name"]))
+            if run_dir not in self.video_readers:
+                self.video_readers[run_dir] = {
+                    f"input_image_{idx}": get_indexed_reader(os.path.join(run_dir, f"{i}.mp4"))
+                    for idx, i in enumerate(self.input_names)
+                }
+            self.index.loc[(self.index["track_name"] == row["track_name"])
+                           & (self.index["subject"] == row["subject"])
+                           & (self.index["run"] == row["run"]), "run_dir"] = run_dir
 
     def __len__(self):
         return len(self.index.index)
@@ -190,6 +219,8 @@ class ImageToControlDataset(Dataset):
         # get the information about the current item
         current_row = self.index.iloc[item]
         current_frame_index = current_row["frame"]
+        current_run_dir = current_row["run_dir"]
+        """
         current_run_dir = os.path.join(self.data_root, run_info_to_path(current_row["subject"],
                                                                         current_row["run"],
                                                                         current_row["track_name"]))
@@ -200,6 +231,7 @@ class ImageToControlDataset(Dataset):
                 f"input_image_{idx}": get_indexed_reader(os.path.join(current_run_dir, f"{i}.mp4"))
                 for idx, i in enumerate(self.input_names)
             }
+        """
 
         # read the input frames
         inputs = [self.video_readers[current_run_dir][f"input_image_{idx}"][current_frame_index]
@@ -275,6 +307,18 @@ class ImageAndStateToControlDataset(Dataset):
         ]) for i in self.video_input_names]
 
         self.video_readers = {}
+        self.index["run_dir"] = None
+        unique_run_info = self.index.groupby(["track_name", "subject", "run"]).size().reset_index()
+        for _, row in unique_run_info.iterrows():
+            run_dir = os.path.join(self.data_root, run_info_to_path(row["subject"], row["run"], row["track_name"]))
+            if run_dir not in self.video_readers:
+                self.video_readers[run_dir] = {
+                    f"input_image_{idx}": get_indexed_reader(os.path.join(run_dir, f"{i}.mp4"))
+                    for idx, i in enumerate(self.video_input_names)
+                }
+            self.index.loc[(self.index["track_name"] == row["track_name"])
+                           & (self.index["subject"] == row["subject"])
+                           & (self.index["run"] == row["run"]), "run_dir"] = run_dir
 
     def __len__(self):
         return len(self.index.index)
@@ -283,6 +327,8 @@ class ImageAndStateToControlDataset(Dataset):
         # get the information about the current item
         current_row = self.index.iloc[item]
         current_frame_index = current_row["frame"]
+        current_run_dir = current_row["run_dir"]
+        """
         current_run_dir = os.path.join(self.data_root, run_info_to_path(current_row["subject"],
                                                                         current_row["run"],
                                                                         current_row["track_name"]))
@@ -294,6 +340,7 @@ class ImageAndStateToControlDataset(Dataset):
                 f"input_image_{idx}": get_indexed_reader(os.path.join(current_run_dir, f"{i}.mp4"))
                 for idx, i in enumerate(self.video_input_names)
             }
+        """
 
         # read the input frames
         video_inputs = [self.video_readers[current_run_dir][f"input_image_{idx}"][current_frame_index]
@@ -327,20 +374,163 @@ class ImageToAttentionAndControlDataset(Dataset):
     pass
 
 
+class StackedImageToAttentionDataset(Dataset):
+
+    def __init__(self, config, split):
+        super().__init__()
+        self.data_root = config["data_root"]
+        self.input_names = config["input_video_names"]
+        self.output_name = config["ground_truth_name"]
+        self.split = split
+        self.stack_size = config["stack_size"]
+
+        frame_index = pd.read_csv(os.path.join(self.data_root, "index", "frame_index.csv"))
+        split_index = pd.read_csv(config["split_config"] + ".csv")
+        frame_index["split"] = split_index
+        self.index = frame_index[frame_index["split"] == self.split].copy().reset_index(drop=True)
+
+        with open(config["split_config"] + "_info.json", "r") as f:
+            split_index_info = json.load(f)
+        input_statistics = {}
+        for i in self.input_names:
+            if "mean" in split_index_info and "std" in split_index_info:
+                input_statistics[i] = {
+                    "mean": split_index_info["mean"][i] if i in split_index_info["mean"] else STATISTICS["mean"][i],
+                    "std": split_index_info["std"][i] if i in split_index_info["std"] else STATISTICS["std"][i]
+                }
+            else:
+                input_statistics[i] = {
+                    "mean": STATISTICS["mean"][i],
+                    "std": STATISTICS["std"][i]
+                }
+        self.input_transforms = [transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(config["resize"]),
+            transforms.ToTensor(),
+            transforms.Normalize(input_statistics[i]["mean"], input_statistics[i]["std"])
+        ]) for i in self.input_names]
+        self.output_transform = transforms.Compose([
+            ImageToAttentionMap(),
+            transforms.ToPILImage(),
+            transforms.Resize(config["resize"]),
+            transforms.ToTensor(),
+            MakeValidDistribution()
+        ])
+        # TODO: MIGHT NEED TO ADD ADDITIONAL STREAM/TRANSFORM/WHATEVER FOR
+        #  THE CROPPED STREAM OF THE COARSE-REFINE ARCHITECTURE
+
+        self.video_readers = {}
+        self.index["run_dir"] = None
+        unique_run_info = self.index.groupby(["track_name", "subject", "run"]).size().reset_index()
+        for _, row in unique_run_info.iterrows():
+            run_dir = os.path.join(self.data_root, run_info_to_path(row["subject"], row["run"], row["track_name"]))
+            if run_dir not in self.video_readers:
+                self.video_readers[run_dir] = {
+                    f"input_stack_{idx}": get_indexed_reader(os.path.join(run_dir, f"{i}.mp4"))
+                    for idx, i in enumerate(self.input_names)
+                }
+                self.video_readers[run_dir]["output_attention"] = get_indexed_reader(os.path.join(
+                    run_dir, f"{self.output_name}.mp4"))
+            self.index.loc[(self.index["track_name"] == row["track_name"])
+                           & (self.index["subject"] == row["subject"])
+                           & (self.index["run"] == row["run"]), "run_dir"] = run_dir
+
+        # find contiguous sequences of frames
+        sequences = []
+        frames = self.index["frame"]
+        jumps = (frames - frames.shift()) != 1
+        frames = list(frames.index[jumps]) + [frames.index[-1] + 1]
+        for i, start_index in enumerate(frames[:-1]):
+            # note that the range [start_frame, end_frame) is exclusive
+            sequences.append((start_index, frames[i + 1]))
+
+        # need some way to not mess up the indexing, but also cannot remove data
+        # maybe use second "index" that skips the (stack_size - 1) first frames of each contiguous sequence?
+        self.index["stack_index"] = -1
+        df_col_index = self.index.columns.get_loc("stack_index")
+        total_num_frame_stacks = 0
+        for start_index, end_index in sequences:
+            num_frames_seq = end_index - start_index
+            num_frame_stacks = num_frames_seq - self.stack_size + 1
+            if end_index - start_index < self.stack_size:
+                continue
+
+            # assign index over entire dataframe to "valid" frames
+            index = np.arange(num_frame_stacks) + total_num_frame_stacks
+            # print(len(index))
+            # print(end_index - (start_index + self.stack_size - 1))
+            # print(len(self.index.iloc[(start_index + self.stack_size - 1):end_index, df_col_index].index))
+            self.index.iloc[(start_index + self.stack_size - 1):end_index, df_col_index] = index
+
+            # keep track of the current number of stacks (highest index)
+            total_num_frame_stacks += num_frame_stacks
+
+    def __len__(self):
+        return len(self.index.index)
+
+    def __getitem__(self, item):
+        # get the information about the current item
+        current_stack_index = self.index.index[self.index["stack_index"] == item].values[0]
+        current_run_dir = self.index["run_dir"].iloc[current_stack_index]
+        current_stack_df = self.index.iloc[(current_stack_index - self.stack_size + 1):(current_stack_index + 1)]
+        current_frame_index = current_stack_df["frame"].iloc[-1]
+
+        # read the frames
+        inputs = [[] for _ in self.input_names]
+        inputs_original = [[] for _ in self.input_names]
+        for idx, _ in enumerate(self.input_names):
+            for _, row in current_stack_df.iterrows():
+                frame = self.video_readers[current_run_dir][f"input_stack_{idx}"][row["frame"]]
+                frame_original = frame.copy()
+
+                # apply input transform here already
+                frame = self.input_transforms[idx](frame)
+
+                inputs[idx].append(frame)
+                inputs_original[idx].append(frame_original)
+
+            # stack the frames
+            # TODO: not sure if this is actually the right dimension to stack them in
+            inputs[idx] = torch.stack(inputs[idx], 1)
+            inputs_original[idx] = np.stack(inputs_original[idx], 0)
+
+        output = self.video_readers[current_run_dir]["output_attention"][current_frame_index]
+
+        # start constructing the output dictionary => keep the original frames in there
+        # out = {"original": {f"input_stack_{idx}": np.array(i.copy()) for idx, i in enumerate(inputs)}}
+        out = {"original": {}}
+        for idx, _ in enumerate(self.input_names):
+            out["original"][f"input_stack_{idx}"] = np.array(inputs_original[idx])
+        out["original"]["output_attention"] = np.array(output)
+
+        # apply transform only for the output
+        for idx, i in enumerate(inputs):
+            out[f"input_stack_{idx}"] = i
+        out["output_attention"] = self.output_transform(output)
+
+        # return a dictionary
+        return out
+
+
 if __name__ == "__main__":
     test_config = {
         "data_root": os.getenv("GAZESIM_ROOT"),
         "input_video_names": ["screen", "hard_mask_moving_window_frame_mean_gt"],
-        "ground_truth_name": "drone_control_frame_mean_gt",
+        "ground_truth_name": "moving_window_frame_mean_gt",  # "drone_control_frame_mean_gt",
         "drone_state_names": ["DroneVelocityX", "DroneVelocityY", "DroneVelocityZ"],
         "resize": 150,
-        "split_config": resolve_split_index_path(10, data_root=os.getenv("GAZESIM_ROOT"))
+        "split_config": resolve_split_index_path(10, data_root=os.getenv("GAZESIM_ROOT")),
+        "stack_size": 4
     }
 
-    dataset = ImageAndStateToControlDataset(test_config, split="train")
+    dataset = StackedImageToAttentionDataset(test_config, split="train")
     print(len(dataset))
     print(dataset.index.columns)
 
     sample = dataset[0]
     print(sample.keys())
     print(sample["original"].keys())
+
+    print(sample["input_stack_0"].shape)
+    print(sample["input_stack_1"].shape)
+    print(sample["output_attention"].shape)
---
+++ src/models/dreyeve.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+
+from src.models.utils import image_log_softmax
+
+# all of this basically taken from https://github.com/ndrplz/dreyeve/blob/master/experiments/train/models.py
+# TODO: maybe consider replacing the C3D parts?
+#  see here for alternatives: https://discuss.pytorch.org/t/pertained-c3d-model-for-video-classification/15506
+
+
+class BilinearUpsampling(nn.Module):
+    pass
+
+
+class CoarseSaliencyModel(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.conv_pool_0 = nn.Sequential(
+            nn.Conv3d(in_channels=3, out_channels=64, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        )
+        # TODO: not sure how to replicate Keras' "same" padding mode in all cases
+        #  => "valid" should just be the default that is done in PyTorch anyway
+
+        # TODO: also not entirely sure about which dimensions the tuples refer to (but I'm guessing the first is
+        #  the temporal dimension) => mostly matters for the max-pooling that uses different values
+
+        self.conv_pool_1 = nn.Sequential(
+            nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        )
+
+        self.conv_pool_2 = nn.Sequential(
+            nn.Conv3d(in_channels=128, out_channels=256, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.Conv3d(in_channels=256, out_channels=256, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        )
+
+        self.conv_pool_3 = nn.Sequential(
+            nn.Conv3d(in_channels=256, out_channels=512, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.Conv3d(in_channels=512, out_channels=512, kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(4, 1, 1), stride=(4, 1, 1))
+        )
+
+        self.upsampling = nn.UpsamplingBilinear2d(scale_factor=8)
+
+    def forward(self, x):
+        x = self.conv_pool_0(x)
+        x = self.conv_pool_1(x)
+        x = self.conv_pool_2(x)
+        x = self.conv_pool_3(x)
+        x = x.reshape(tuple(x.shape[:2]) + tuple(x.shape[3:]))
+        x = self.upsampling(x)
+        return x
+
+
+class SaliencyBranch(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+        self.coarse_predictor = CoarseSaliencyModel(config)
+
+        self.upsampling_coarse_to_fine = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.UpsamplingBilinear2d(scale_factor=4)
+        )
+
+        self.conv_refine_0 = nn.Sequential(
+            nn.Conv2d(in_channels=4, out_channels=32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(negative_slope=0.001)
+        )
+
+        self.conv_refine_1 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(negative_slope=0.001)
+        )
+
+        self.conv_refine_2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.LeakyReLU(negative_slope=0.001)
+        )
+
+        self.conv_refine_3 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=1, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU()
+        )
+
+        self.conv_output_crop = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        full_x = self.coarse_predictor(x["stack_full"])
+        print(full_x.shape)
+        full_x = self.upsampling_coarse_to_fine(full_x)
+        print(full_x.shape)
+        full_x = torch.cat([full_x, x["image_full"]], dim=1)
+        print(full_x.shape)
+        full_x = self.conv_refine_0(full_x)
+        print(full_x.shape)
+        full_x = self.conv_refine_1(full_x)
+        print(full_x.shape)
+        full_x = self.conv_refine_2(full_x)
+        print(full_x.shape)
+        full_x = self.conv_refine_3(full_x)
+        print(full_x.shape)
+        print()
+
+        crop_x = self.coarse_predictor(x["stack_crop"])
+        print(crop_x.shape)
+        crop_x = self.conv_output_crop(crop_x)
+        print(crop_x.shape)
+
+        out = {
+            "full": full_x,
+            "crop": crop_x
+        }
+        return out
+
+
+class DrEYEveNet(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        pass
+
+
+if __name__ == "__main__":
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    """
+    image = torch.ones((2, 3, 150, 200)).to(device)
+    state = torch.ones((2, 9)).to(device)
+    label = torch.LongTensor([0, 1]).to(device)
+    X = {
+        "input_image_0": image,
+        "input_image_1": image.clone(),
+        "input_state": state,
+        "label_high_level": label
+    }
+    """
+    X = {
+        "stack_full": torch.zeros((1, 3, 16, 112, 112)).to(device),
+        "stack_crop": torch.zeros((1, 3, 16, 112, 112)).to(device),
+        "image_full": torch.zeros((1, 3, 448, 448)).to(device)
+    }
+
+    net = SaliencyBranch({}).to(device)
+    result = net(X)
+    # print(result)
+
